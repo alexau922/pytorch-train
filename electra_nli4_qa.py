05bc94c8c409812a6b11931d11c2a1a1bf78d9b8
alex-au-922 <79151747+alex-au-922@users.noreply.github.com>
2021-07-28 12:32:18 +0800

    Update electra_nli4_qa.py

--- electra_nli4_qa.py
+++ electra_nli4_qa.py
@@ -469,7 +469,7 @@ def post_evaluate(record, args):
     record['correct_tot'] = record['correct_tot']
 
 def log_formatter(log, tb, step_i):
-    log['acc'] = float(log['acc'] / log['acc_tot'])
+    log['overall_acc'] = float(torch.mean(torch.stack([log['start_acc'],log['end_acc'],log['answerability_acc']],dim=0)))
 
 def get_tokenizer(args):
     return None
